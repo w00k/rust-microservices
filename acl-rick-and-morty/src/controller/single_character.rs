1346feaad8971ae1b261ea6bcc8d7db896453801
Francisco Arenas <franciscoarenas.globant@latam.com>
2023-06-26 02:48:33 -0400

    remove bad code

--- acl-rick-and-morty/src/controller/single_character.rs
+++ acl-rick-and-morty/src/controller/single_character.rs
@@ -5,14 +5,14 @@ use crate::service;
 #[get("/single-character/{character_id}")]
 async fn single_character_controller(path: web::Path<String>) -> impl Responder {
     let character_id = path.into_inner();
-    let response = service::rick_and_morty_api_character(character_id);
+    let response = service::single_character_service::rick_and_morty_call_api_character(character_id);
     HttpResponse::Ok().json(response)
 }
 
 #[get("/single-character-two/{character_id}")]
 async fn single_character_controller_two(path: web::Path<String>) -> impl Responder {
     let character_id = path.into_inner();
-    let response = service::rick_and_morty_api_character_two(character_id);
+    let response = service::single_character_service::rick_and_morty_call_api_character_two(character_id);
     
     if response.is_err() {
         let message_error = response.err().unwrap();
--- acl-rick-and-morty/src/service/mod.rs
+++ acl-rick-and-morty/src/service/mod.rs
@@ -1,12 +1,1 @@
-use crate::model::{rick_and_morty_single_character::Character, error_message::CustomError};
-
-mod single_character_service;
-
-pub fn rick_and_morty_api_character(character_id: String) -> Character {
-    println!("mod service ID {}", character_id);
-    single_character_service::rick_and_morty_call_api_character(character_id)
-}
-
-pub fn rick_and_morty_api_character_two(character_id: String) -> Result<Character, CustomError> {
-    single_character_service::rick_and_morty_call_api_character_two(character_id)
-}
+pub mod single_character_service;
